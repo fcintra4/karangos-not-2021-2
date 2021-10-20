c28d2a3c42a38a974169fbe64848536a4e963b52
GustavoSalga <gustavo.vieira16@fatec.sp.gov.br>
2021-10-20 01:29:07 +0000

    (Aula 19/10) Início do Projeto Karangos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AppHeader from './userInterface/AppHeader';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { yellow, pink } from '@mui/material/colors';
+
+import ClientesList from './routed/ClientesList'
+import ClientesForm from './routed/ClientesForm'
+
+const customTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: yellow[500]
+    },
+    secondary: {
+      main: pink[500]
+    }
+  }
+})
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <ThemeProvider theme={customTheme}>
+    <AppHeader />
+    <Switch>
+
+      <Route path="/clientes" exact>
+        <ClientesList />
+      </Route>
+
+      <Route path="/clientes/new">
+        <ClientesForm />
+      </Route>
+
+    </Switch>
+    </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
