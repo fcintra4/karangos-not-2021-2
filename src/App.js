d6dee7fea0a203b80bbe04b00d438253f6be0d74
si39 <sijuta01@gmail.com>
2021-10-20 01:30:23 +0000

    (19/10) Início do projeto Karangos

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
+import AppHeader from './ui/AppHeader'
+import { createTheme, ThemeProvider } from '@mui/material';
+import { yellow, pink } from '@mui/material/colors';
+
+import ClientesList from './routed/ClientesList'
+import ClientesForm from './routed/ClientesForm'
+
+const customTheme = createTheme({
+   palette:{
+    node: 'dark',
+    primary: {
+      main: yellow[500]
+    },
+    secondary:{
+      main: pink[500]
+    }
+  }
+
+})
+
+function App() {
+  return (
+    <BrowserRouter>
+    <ThemeProvider theme={customTheme}>
+    <AppHeader bgColorcolor={customTheme.palette.primary.main} />
+
+    <Switch>
+
+      <Route path="/clientes" exact>
+        <ClientesList />
+          </Route>
+
+          <Route path="/clientes/new">
+        <ClientesForm/>
+          </Route>
+
+          </Switch>
+    </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
