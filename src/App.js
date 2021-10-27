b12146948106d7a92fd899a67801cac4d4fd8556
dripivetta <dripivetta@hotmail.com>
2021-10-27 01:26:35 +0000

    (26/10)Rodapé e início do componente de Listagem

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { createTheme, ThemeProvider } from '@mui/material';
 import { yellow, pink } from '@mui/material/colors';
 import ClientesList from './routed/ClientesList'
 import ClientesForm from './routed/ClientesForm'
+import AppFooter from './UI/AppFooter';
+import Box from '@mui/material/Box'
 
 const customTheme = createTheme ({
   palette:{
@@ -22,18 +24,33 @@ function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={customTheme}>
-        <AppHeader/>
-        <Switch>
 
-          <Route path ="clientes" exact>
-            <ClientesList/>
-          </Route>
+        <Box sx={{ 
+            minHeight : '100vh', //100% da altura da área de exibição
+            backgroundColor:customTheme.palette.background.default,
+            color: customTheme.palette.text.primary
+          }}>
 
-          <Route path ="clientes/new">
-            <ClientesForm/>
-          </Route>
+          <AppHeader/>
 
-        </Switch>
+          <Switch>
+
+            <Box compoente="main" sx={{margin: '20px 20px 60px 20px'}}>
+              <Route path ="/clientes" exact>
+                <ClientesList/>
+              </Route>
+
+              <Route path ="/clientes/new">
+                <ClientesForm/>
+              </Route>
+            </Box>
+
+          </Switch> 
+
+          <AppFooter/>
+
+        </Box>
+
       </ThemeProvider>
     </BrowserRouter>
   );
