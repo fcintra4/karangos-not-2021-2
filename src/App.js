eb48a095faa6dc63c3ee917f7ec0d4c531e86572
pedrosoale <profe.pedroso@gmail.com>
2021-11-09 22:01:06 +0000

    (26/10) Rodapé e início do componente de listagem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import AppHeader from './ui/AppHeader';
+import AppHeader from './ui/AppHeader'
+import AppFooter from './ui/AppFooter'
 import { createTheme, ThemeProvider } from '@mui/material';
 import { yellow, pink } from '@mui/material/colors';
+import Box from '@mui/material/Box'
 
-import ClientesList from './routed/ClientesList';
-import ClientesForm from './routed/ClientesForm';
+import ClientesList from './routed/ClientesList'
+import ClientesForm from './routed/ClientesForm'
 
 const customTheme = createTheme({
   palette: {
@@ -23,19 +25,26 @@ function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={customTheme}>
-        <AppHeader />
-        <Switch>
+        <Box sx={{ 
+          minHeight: '100vh', // 100% da altura da área de exibição
+          backgroundColor: customTheme.palette.background.default,
+          color: customTheme.palette.text.primary
+        }}>
+          <AppHeader />
+          <Switch>
+            <Box component="main" sx={{ margin: '20px 20px 60px 20px'}}>
+              <Route path="/clientes" exact>
+                <ClientesList />
+              </Route>
 
-          <Route path="/clientes" exact>
-            <ClientesList />
-          </Route>
-
-          <Route path="/clientes/new">
-            <ClientesForm />
-          </Route>
-
-        </Switch>
-      </ ThemeProvider>
+              <Route path="/clientes/new">
+                <ClientesForm />
+              </Route>
+            </Box>
+          </Switch>
+          <AppFooter />
+        </Box>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
