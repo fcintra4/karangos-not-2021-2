4ff04b0b3a0dbdef233eb1bba20b85f07039e889
ppconrado <ppconrado@yahoo.com.br>
2021-11-19 05:40:48 -0300

    clientsForm init + ClientList done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import AppHeader from './ui/AppHeader'
-import AppFooter from './ui/AppFooter'
-import { createTheme, ThemeProvider } from '@mui/material';
-import { yellow, pink } from '@mui/material/colors';
-import Box from '@mui/material/Box'
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AppHeader from "./ui/AppHeader";
+import AppFooter from "./ui/AppFooter";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { yellow, pink } from "@mui/material/colors";
+import Box from "@mui/material/Box";
 
-import ClientesList from './routed/ClientesList'
-import ClientesForm from './routed/ClientesForm'
+import ClientesList from "./routed/ClientesList";
+import ClientesForm from "./routed/ClientesForm";
 
 const customTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: yellow[500]
+      main: yellow[500],
     },
     secondary: {
-      main: pink[500]
-    }
-  }
-})
+      main: pink[500],
+    },
+  },
+});
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={customTheme}>
-        <Box sx={{ 
-          minHeight: '100vh', // 100% da altura da área de exibição
-          backgroundColor: customTheme.palette.background.default,
-          color: customTheme.palette.text.primary
-        }}>
+        <Box
+          sx={{
+            minHeight: "100vh", // 100% da altura da área de exibição
+            backgroundColor: customTheme.palette.background.default,
+            color: customTheme.palette.text.primary,
+          }}
+        >
           <AppHeader />
-          <Switch>
-            <Box component="main" sx={{ margin: '20px 20px 60px 20px'}}>
+          <Box component="main" sx={{ margin: "20px 20px 60px 20px" }}>
+            <Switch>
+              {/* Rota para o componente de listagem */}
               <Route path="/clientes" exact>
                 <ClientesList />
               </Route>
 
-              <Route path="/clientes/new">
+              {/* Rota para o componente de formulário, para cadastrar novo cliente */}
+              <Route path="/clientes/new" exact>
                 <ClientesForm />
               </Route>
-            </Box>
-          </Switch>
+
+              {/* Rota para o componente de formulário, para editar um cliente existente.
+               :id é um PARÂMETRO da rota, que será substituído pelo id real do cliente. */}
+              <Route path="/clientes/:id">
+                <ClientesForm />
+              </Route>
+            </Switch>
+          </Box>
+
           <AppFooter />
         </Box>
       </ThemeProvider>
