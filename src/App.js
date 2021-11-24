e05d534043fd502078b793007efb6af82af8a8e3
Vinicius Ávila <viniciusvaln10@hotmail.com>
2021-11-24 02:41:57 +0000

    Inicio do Karangos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AppHeader from './ui/AppHeader'
-import AppFooter from './ui/AppFooter'
 import { createTheme, ThemeProvider } from '@mui/material';
 import { yellow, pink } from '@mui/material/colors';
-import Box from '@mui/material/Box'
 
 import ClientesList from './routed/ClientesList'
 import ClientesForm from './routed/ClientesForm'
 
 const customTheme = createTheme({
   palette: {
-    mode: 'dark',
     primary: {
       main: yellow[500]
     },
@@ -25,36 +22,17 @@ function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={customTheme}>
-        <Box sx={{ 
-          minHeight: '100vh', // 100% da altura da área de exibição
-          backgroundColor: customTheme.palette.background.default,
-          color: customTheme.palette.text.primary
-        }}>
-          <AppHeader />
-          <Box component="main" sx={{ margin: '20px 20px 60px 20px'}}>
-            <Switch>
-                          
-              {/* Rota para o componente de listagem */}
-              <Route path="/clientes" exact>
-                <ClientesList />
-              </Route>
+        <AppHeader />
+        <Switch>
 
-              {/* Rota para o componente de formulário, para cadastrar novo cliente */}
-              <Route path="/clientes/new" exact>
-                <ClientesForm />
-              </Route>
+          <Route path="/clientes" exact>
+            <ClientesList />
+          </Route>
 
-              {/* Rota para o componente de formulário, para editar um cliente existente.
-               :id é um PARÂMETRO da rota, que será substituído pelo id real do cliente. */}
-              <Route path="/clientes/:id">
-                <ClientesForm />
-              </Route>
-
-            </Switch>
-          </Box>
-          
-          <AppFooter />
-        </Box>
+          <Route path="/clientes/new">
+            <ClientesForm />
+          </Route>
+        </Switch>
       </ThemeProvider>
     </BrowserRouter>
   );
