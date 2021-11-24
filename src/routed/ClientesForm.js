1ba6a41835456b37c75d3f89e3901d229e63f029
heliobarolo0586 <heliobarolo0586@gmail.com>
2021-11-24 01:19:49 +0000

    (23/11) Termino do formulario para cadastro de novo Cliente

--- src/routed/ClientesForm.js
+++ src/routed/ClientesForm.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { TextField } from '@mui/material'
+import TextField from '@mui/material/TextField'
 import { makeStyles } from '@mui/styles'
 import InputMask from 'react-input-mask'
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
@@ -12,6 +12,11 @@ import Button from '@mui/material/Button'
 import validator from 'validator'
 import { validate as cpfValidate } from 'gerador-validador-cpf'
 import { isFuture as dateIsFuture, isValid as dateIsValid } from 'date-fns'
+import axios from 'axios'
+import Snackbar from '@mui/material/Snackbar'
+import { useHistory } from 'react-router-dom'
+import ConfirmDialog from '../ui/ConfirmDialog'
+
 
 const useStyles = makeStyles(theme => ({
     form: {
@@ -32,7 +37,6 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
-
 const unidadesFed = [
     { sigla: 'DF', nome: 'Distrito Federal' },
     { sigla: 'ES', nome: 'Espírito Santo' },
@@ -44,123 +48,227 @@ const unidadesFed = [
     { sigla: 'SP', nome: 'São Paulo' }
 ]
 
-
+const formatChars = {
+    '9': '[0-9]', // Entrada obrigatória
+    '?': '[0-9]?' // Entrada opcional
+}
 
 export default function ClientesForm() {
 
-    const classes = useStyles();
+    const classes = useStyles()
+    const history = useHistory()
 
     const [state, setState] = React.useState({
-        cliente: {}, // Objeto vazio 
+        cliente: {},   // Objeto vazio
         errors: {},
-        inFormValid: false
-
+        isFormValid: false,
+        isSnackOpen: false,
+        snackMessage: '',
+        isServerError: false,
+        sendBtnLabel: 'Enviar',
+        isDialogOpen: false
     })
-
-
-
-    const { cliente, errors, isFormValid } = state
+    const { cliente, errors, isFormValid, isSnackOpen, snackMessage,
+         isServerError, sendBtnLabel, isDialogOpen } = state
 
     function handleInputChange(event, field = event.target.id) {
-        //Depuração 
-        console.log({ event })
-        //Preenche a variável de estado "Cliente"   
-        //Com os valores dos inputs 
+        // Depuração
+        console.log(event)
+
+        // Preenche a variável de estado "cliente"
+        // com os valores dos inputs
         const newCliente = { ...cliente }
 
         if (field === 'data_nascimento') newCliente[field] = event
         else newCliente[field] = event.target.value
 
-        //chama a validação do formulario
+        // Chama a validação do formulário
         const newErrors = formValidate(newCliente)
+        const newIsFormValid = Object.keys(newErrors).length === 0  // Sem erros
 
-        setState({ ...state, clientes: newCliente })
-
-        newCliente[field] = event.target.value
-        setState({ ...state, cliente: newCliente, errors: newErrors })
+        setState({ ...state, cliente: newCliente, errors: newErrors, isFormValid: newIsFormValid })
     }
 
     function formValidate(fields) {
+
         const newErrors = {}
-            //Validação do campo "nome" no mínimo 5 caracteres, devendo ter pelo menos 
-            //um espaço em branco entre eles
-            if (!fields.nome || !(validator.isLength(fields.nome.trim(), {min: 5})
-            && validator.contains(fields.nome.trim(), ' '))){
-                newErrors.nome = 'Informe o nome completo'
-            }
 
-            //Validação do campo "cpf" deve ser válido
-            if (!fields.cpf || !cpfValidate(fields.cpf)){
-                newErrors.cpf = 'CPF inválido'
-            }
+        // Validação do campo "nome": no mínimo 5 caracteres, devendo ter pelo
+        // menos um espaço em branco entre eles
+        if (!fields.nome || !(validator.isLength(fields.nome.trim(), { min: 5 })
+            && validator.contains(fields.nome.trim(), ' '))) {
+            newErrors.nome = 'Informe o nome completo'
+        }
 
-            //Validação do campo RG, no mínimo 4 caracteres
-            if(!fields.rg || !validator.isLength(fields.rg.trim(), {min: 4})){
-                newErrors.rg = 'Doc. identidade incompleto ou não informado'
-            }
+        // Validação do campo "cpf": deve ser válido
+        if (!fields.cpf || !cpfValidate(fields.cpf)) {
+            newErrors.cpf = 'CPF inválido'
+        }
 
-            //Validação do campo "data_nascimento": data deve ser válida e não pode ser futura
-            if (!fields.data_nascimento || !dateIsValid(fields.data_nascimento) || 
-            dateIsFuture(fields.data_nascimento)){
-                newErrors.data_nascimento = 'Data de nascimento inválida ou no futuro'
-            }
+        // Validação do campo "rg": no mínimo, 4 caracteres
+        if (!fields.rg || !validator.isLength(fields.rg.trim(), { min: 4 })) {
+            newErrors.rg = 'Doc. identidade incompleto ou não informado'
+        }
 
-            //Validação do campo "Logradouro": no mínimo, 4 caracteres
-            if(!fields.logradouro || !validator.isLength(fields.logradouro.trim(), {min: 4})){
-                newErrors.logradouro = 'Logradouro incompleto ou não informado'
-            } 
+        // Validação do campo "data_nascimento": data deve ser válida e não pode ser futura
+        if (!fields.data_nascimento || !dateIsValid(fields.data_nascimento) ||
+            dateIsFuture(fields.data_nascimento)) {
+            newErrors.data_nascimento = 'Data de nascimento inválida ou no futuro'
+        }
 
-            //Validação do campo "num_imovel": no mínimo, 1 caracter
-            if(!fields.num_imovel || !validator.isLength(fields.num_imovel.trim(), {min: 1})){
-                newErrors.num_imovel = 'Número do imóvel incompleto ou não informado'
-            } 
+        // Validação do campo "logradouro": no mínimo, 4 caracteres
+        if (!fields.logradouro || !validator.isLength(fields.logradouro.trim(), { min: 4 })) {
+            newErrors.logradouro = 'Logradouro incompleto ou não informado'
+        }
 
-             //Validação do campo "bairro": no mínimo, 3 caracteres
-             if(!fields.bairro || !validator.isLength(fields.bairro.trim(), {min: 3})){
-                newErrors.bairro = 'Bairro incompleto ou não informado'
-            } 
+        // Validação do campo "num_imovel": no mínimo, 1 caracter
+        if (!fields.num_imovel || !validator.isLength(fields.num_imovel.trim(), { min: 1 })) {
+            newErrors.num_imovel = 'Número do imóvel incompleto ou não informado'
+        }
 
-            //Validação do campo "munínicipio": no mínimo, 3 caracteres
-            if(!fields.municipio || !validator.isLength(fields.municipio.trim(), {min: 3})){
-                newErrors.municipio = 'Município incompleto ou não informado'
-            } 
+        // Validação do campo "bairro": no mínimo, 3 caracteres
+        if (!fields.bairro || !validator.isLength(fields.bairro.trim(), { min: 3 })) {
+            newErrors.bairro = 'Bairro incompleto ou não informado'
+        }
 
-            //Validação do campo "uf": preenchido com Exatamente 2 Caracteres
-            if(!fields.uf || !validator.isLength(fields.uf.trim(), {min: 2, max: 2})){
-                newErrors.uf = 'Selecione a UF'
-            } 
+        // Validação do campo "município": no mínimo, 3 caracteres
+        if (!fields.municipio || !validator.isLength(fields.municipio.trim(), { min: 3 })) {
+            newErrors.municipio = 'Município incompleto ou não informado'
+        }
 
-            //Validação do campo "telefone": não pode conter caracteres de sublinhado
-            //(preenchido incompleto)
-            if(!fields.telefone || validator.contains(fields.telefone, '_')){
-                newErrors.telefone = 'Telefone incompleto ou não informado'
-            }
+        // Validação do campo "uf": preenchido com EXATAMENTE 2 caracteres
+        if (!fields.uf || !validator.isLength(fields.uf.trim(), { min: 2, max: 2 })) {
+            newErrors.uf = 'Selecione a UF'
+        }
 
-            //Validate do compo "e-mail": deve ser válido
-            if(!fields.email || !validator.isEmail(fields.email)){
-                newErrors.email = 'Email inválido ou não informado'
-            }
-            
+        // Validação do campo "telefone": não pode conter caracteres de sublinhado
+        // (preenchimento incompleto)
+        if (!fields.telefone || validator.contains(fields.telefone, '_')) {
+            newErrors.telefone = 'Telefone incompleto ou não informado'
+        }
+
+        // Validação do campo "email": deve ser válido
+        if (!fields.email || !validator.isEmail(fields.email)) {
+            newErrors.email = 'Email inválido ou não informado'
+        }
 
         return newErrors
 
     }
 
     function handleSubmit(event) {
-        //Evita o recarregamento da página após o envio do formulário
+
+        // Evita o recarregamento da página após o envio do formulário
         event.preventDefault()
 
-        //TODO: salvar os dados no servidor se os dados estiverem válidos
+        // Salva os dados no servidor se o formulário estiver válido
+        if (isFormValid) saveData()
+
     }
 
+    function isFormTouched() {
+        //Percorrer o objeto "cliente" para ver se houve alteração nos]
+        //campos do formulário
+        for (let field in cliente) {
+            //Há pelo menos um campo com conteúdo
+            if (cliente[field] !== '') return true
+        }
+
+        return false
+    }
+
+    function saveData() {
+
+        // Muda o texto do botão de enviar e o desabilita, para evitar envios repetidos
+        setState({ ...state, sendBtnLabel: 'Enviando...' })
+
+        axios.post('https://api.faustocintra.com.br/clientes', cliente)
+            .then(
+                // Callback se der certo
+                () => {
+                    setState({
+                        ...state,
+                        isSnackOpen: true,
+                        snackMessage: 'Dados salvos com sucesso.',
+                        isServerError: false,
+                        sendBtnLabel: 'Enviar'
+                    })
+                }
+            )
+            .catch(
+                // Callback se der errado
+                error => {
+                    setState({
+                        ...state,
+                        isSnackOpen: true,
+                        snackMessage: 'ERRO: ' + error.message,
+                        isServerError: true,
+                        sendBtnLabel: 'Enviar'
+                    })
+                }
+            )
+    }
+
+    function handleSnackClose(event, reason) {
+        // Evita que o snackbar seja fechado clicando-se fora dele 
+        if (reason === 'clickaway') return
+
+        // Fechamento em condições normais
+        setState({ ...state, isSnackOpen: false })
+
+        // Quando não há erro de servidor, após o fechamento do snackbar
+        // retornamos ao componente de listagem
+        if (!isServerError) history.push('/clientes')
+    }
+
+    function handleDialogClose(answer){
+        //Se o usuário responder OK à perrgunta, volta
+        //para a página anterior (mesmo perdendo os dados)
+        if(answer) history.goBack()
+
+        setState({...state, isDialogOpen: false}) //Fecha a caixa de dialogo
+    }
+
+    function handleBackBtnClick(){
+
+        //Se o formulário estiver alterado, é necessário
+        //perguntar se o usuário realmente quer voltar
+
+        if(isFormTouched()) setState({...state, isDialogOpen: true})
+
+        else history.goBack()
+    }
 
     return (
         <>
-            <h1>Cadastrar novo Cliente</h1> 
+            <h1>Cadastrar novo cliente</h1>
+
+            <Snackbar
+                open={isSnackOpen}
+                autoHideDuration={6000}
+                onClose={handleSnackClose}
+                message={snackMessage}
+                action={
+                    <Button color="secondary" size="small" onClick={handleSnackClose}>
+                        {isServerError ? 'Que pena!' : 'Entendi'}
+                    </Button>
+                }
+            />
+
+            <ConfirmDialog
+                title="ATENÇÃO: possível perda de daddos"
+                open={isDialogOpen}
+                onClose={handleDialogClose}
+            >
+
+                Há dados ainda não salvos. Deseja realmente voltar ?
+            </ConfirmDialog>
+
             <form className={classes.form} onSubmit={handleSubmit}>
+
                 <TextField
                     id="nome"
-                    label="Nome Completo"
+                    label="Nome completo"
                     variant="filled"
                     value={cliente.nome}
                     required
@@ -185,26 +293,10 @@ export default function ClientesForm() {
                             fullWidth
                             placeholder="Informe o CPF do cliente"
                             helperText={errors?.cpf}
-                            error={errors?.cpfS}
+                            error={errors?.cpf}
                         />
                     }
                 </InputMask>
-
-                {/* 
-
-                <TextField
-                    value={cliente.cpf}
-                    onChange={handleInputChange}
-                    id="cpf"
-                    label="CPF"
-                    variant="filled"
-                    required
-                    fullWidth
-                    placeholder="Informe o CPF do cliente"
-                />
-                */}
-
-
 
                 <TextField
                     id="rg"
@@ -215,11 +307,14 @@ export default function ClientesForm() {
                     fullWidth
                     placeholder="Informe o documento de identidade do cliente"
                     onChange={handleInputChange}
+                    helperText={errors?.rg}
+                    error={errors?.rg}
+
                 />
 
                 <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptLocale}>
                     <DatePicker
-                        label="Data de Nascimento"
+                        label="Data de nascimento"
                         value={cliente.data_nascimento}
                         onChange={event => handleInputChange(event, 'data_nascimento')}
                         renderInput={(params) => <TextField
@@ -227,6 +322,8 @@ export default function ClientesForm() {
                             id="data_nascimento"
                             variant="filled"
                             fullWidth
+                            helperText={errors?.data_nascimento}
+                            error={errors?.data_nascimento}
                         />
                         }
                     />
@@ -241,6 +338,8 @@ export default function ClientesForm() {
                     fullWidth
                     placeholder="Rua, avenida, etc."
                     onChange={handleInputChange}
+                    helperText={errors?.logradouro}
+                    error={errors?.logradouro}
                 />
 
                 <TextField
@@ -251,6 +350,8 @@ export default function ClientesForm() {
                     required
                     fullWidth
                     onChange={handleInputChange}
+                    helperText={errors?.num_imovel}
+                    error={errors?.num_imovel}
                 />
 
                 <TextField
@@ -271,16 +372,20 @@ export default function ClientesForm() {
                     required
                     fullWidth
                     onChange={handleInputChange}
+                    helperText={errors?.bairo}
+                    error={errors?.bairro}
                 />
 
                 <TextField
                     id="municipio"
-                    label="Municipio"
+                    label="Município"
                     variant="filled"
                     value={cliente.municipio}
                     required
                     fullWidth
                     onChange={handleInputChange}
+                    helperText={errors?.municipio}
+                    error={errors?.municipio}
                 />
 
                 <TextField
@@ -290,12 +395,11 @@ export default function ClientesForm() {
                     value={cliente.uf}
                     required
                     fullWidth
-                    onChange={handleInputChange}
+                    onChange={event => handleInputChange(event, 'uf')}
                     select
+                    helperText={errors?.uf}
+                    error={errors?.uf}
                 >
-
-
-
                     {
                         unidadesFed.map(uf => (
                             <MenuItem key={uf.sigla} value={uf.sigla}>
@@ -305,23 +409,66 @@ export default function ClientesForm() {
                     }
                 </TextField>
 
+                <InputMask
+                    mask="(99) ?9999-9999"
+                    formatChars={formatChars}
+                    value={cliente.telefone}
+                    onChange={handleInputChange}
+                >
+                    {
+                        () => <TextField
+                            id="telefone"
+                            label="Telefone"
+                            variant="filled"
+                            required
+                            fullWidth
+                            placeholder="Informe o telefone do cliente"
+                            helperText={errors?.telefone}
+                            error={errors?.telefone}
+                        />
+                    }
+                </InputMask>
+
+                <TextField
+                    id="email"
+                    label="E-mail"
+                    variant="filled"
+                    value={cliente.email}
+                    required
+                    fullWidth
+                    onChange={handleInputChange}
+                    helperText={errors?.email}
+                    error={errors?.email}
+                />
+
                 <Toolbar className={classes.toolbar}>
                     <Button
                         variant="contained"
                         color="secondary"
-                        type="submit">
-                        Enviar
+                        type="submit"
+                        disabled={sendBtnLabel !== 'Enviar'}
+                    >
+                        {sendBtnLabel}
                     </Button>
-                    <Button
-                        variant="outlined">
-                        Voltar
-                    </Button>
+                    <Button variant="outlined" onClick={handleBackBtnClick}>Voltar</Button>
                 </Toolbar>
 
             </form>
+
             <div>
                 {JSON.stringify(cliente)}
             </div>
+
+            {/* 
+      <div>
+        {JSON.stringify(errors)}
+      </div>
+      */}
+
+            <div>
+                {'Formulario alterado: ' + isFormTouched()}
+            </div>
+
         </>
     )
 }
