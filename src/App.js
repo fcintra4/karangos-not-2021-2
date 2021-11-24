486dff5e29955fd06549b3b5676cb1aa5b1f0a4b
Carlos Cezar De Souza <66181262+CarlosCezarDeSouza@users.noreply.github.com>
2021-11-23 22:21:42 -0300

    23/11 finalização do projeto

--- src/App.js
+++ src/App.js
@@ -27,11 +27,12 @@ function App() {
       <ThemeProvider theme={customTheme}>
         <Box sx={{ 
           minHeight: '100vh', // 100% da altura da área de exibição
+          marginBottom: '40px',
           backgroundColor: customTheme.palette.background.default,
           color: customTheme.palette.text.primary
         }}>
           <AppHeader />
-          <Box component="main" sx={{ margin: '20px 20px 60px 20px'}}>
+          <Box component="main" sx={{ margin: '20px'}}>
             <Switch>
                           
               {/* Rota para o componente de listagem */}
