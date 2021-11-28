8adab9cf6dd6a4372f0b0e3fff3961f4334cfdcb
Nicole Molina <pvrge@hotmail.com>
2021-11-28 06:07:38 -0300

    (26/10) Rodapé e início do componente de listagem

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { yellow, pink } from '@mui/material/colors';
+import Box from '@mui/material/Box';
 import AppHeader from './ui/AppHeader';
+import AppFooter from './ui/AppFooter';
 import ClientesList from './routed/ClientesList';
 import ClientesForm from './routed/ClientesForm';
 
@@ -22,11 +24,22 @@ function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={customTheme}>
-        <AppHeader />
-        <Routes>
-          <Route path="/clientes" element={<ClientesList />} exact />
-          <Route path="/clientes/new" element={<ClientesForm />} />
-        </Routes>
+        <Box
+          sx={{
+            minHeight: '100vh', // 100% da altura da área de exibição
+            backgroundColor: customTheme.palette.background.default,
+            color: customTheme.palette.text.primary,
+          }}
+        >
+          <AppHeader />
+          <Box component="main" sx={{ margin: '20px 20px 60px 20px' }}>
+            <Routes>
+              <Route path="/clientes" element={<ClientesList />} />
+              <Route path="/clientes/new" element={<ClientesForm />} />
+            </Routes>
+          </Box>
+          <AppFooter />
+        </Box>
       </ThemeProvider>
     </BrowserRouter>
   );
