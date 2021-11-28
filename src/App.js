0531aed29b7b30e69ec101ac8d5a8d386840a129
pedrosoale <profe.pedroso@gmail.com>
2021-11-28 12:57:15 +0000

    (28/11) Trabalho 2 - Listagem de Veículos

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Box from '@mui/material/Box'
 
 import ClientesList from './routed/ClientesList'
 import ClientesForm from './routed/ClientesForm'
+import KarangosList from './routed/KarangosList';
 
 const customTheme = createTheme({
   palette: {
@@ -25,15 +26,15 @@ function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={customTheme}>
-        <Box sx={{ 
+        <Box sx={{
           minHeight: '100vh', // 100% da altura da área de exibição
           backgroundColor: customTheme.palette.background.default,
           color: customTheme.palette.text.primary
         }}>
           <AppHeader />
-          <Box component="main" sx={{ margin: '20px 20px 60px 20px'}}>
+          <Box component="main" sx={{ margin: '20px 20px 60px 20px' }}>
             <Switch>
-                          
+
               {/* Rota para o componente de listagem */}
               <Route path="/clientes" exact>
                 <ClientesList />
@@ -50,9 +51,14 @@ function App() {
                 <ClientesForm />
               </Route>
 
+              {/* Rota para o componente de listagem de veículos */}
+              <Route path="/karangos" exact>
+                <KarangosList />
+              </Route>
+
             </Switch>
           </Box>
-          
+
           <AppFooter />
         </Box>
       </ThemeProvider>
